250d0b4b62d8a1c983ea7eaec18068926f694865
shaun <shaun.weller96@gmail.com>
2019-01-16 16:08:55 +0000

    If app is run for the first time, the profile page opens.
    If not first time, main activity fragment opens.

--- app/src/main/java/com/weller/justlift/MainActivity.java
+++ app/src/main/java/com/weller/justlift/MainActivity.java
@@ -1,5 +1,7 @@
 package com.weller.justlift;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,15 +13,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     ProfileDB myDB;
     TextView nameTextView;
+
+    private Boolean isFirst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isFirst = firstCheck();
+        if (isFirst) {
+            //startActivity(new Intent(MainActivity.this, NutritionFragment.class));
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ProfileFragment()).commit();
+            isFirst = false;
+        }
+        getSharedPreferences("firstRun", MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).commit();
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -34,14 +48,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        myDB = new ProfileDB(this);//this isn't working yet, table isnt creating
+        //myDB = new ProfileDB(this);//this isn't working yet, table isnt creating
         //oncreate will open homefragment
 
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()){ //switch statement finds the clicked menu item by the id
+        switch (menuItem.getItemId()) { //switch statement finds the clicked menu item by the id
             case R.id.nav_home:
                 //code executed when id workout clicked
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -79,15 +93,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }
-        else {
+        } else {
             super.onBackPressed();
         }
     }
 
-    public void isProfile(){
-
+    public boolean firstCheck() {
+        if (isFirst == null) {//if isFirst hasn't been initialised (this occurs on first run)
+            SharedPreferences mPreferences = this.getSharedPreferences("firstRun", MODE_PRIVATE); //gets shared preference tag firstRun
+            isFirst = mPreferences.getBoolean("firstRun", true);//sets it to true and returns firstRun as true
+            if (isFirst) {//if isFirst set to true
+                SharedPreferences.Editor editor = mPreferences.edit();
+                editor.putBoolean("firstRun", false);//edits to false
+                editor.commit();
+            }
+        }
+        return isFirst;//returns if it's the first time app has been run
     }
+
 
 /*    @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
